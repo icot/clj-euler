9ee735af65e6d60fde70b8fc592cce2d0039c893
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2020-11-26 07:42:17 +0100

    add naive tail recursive factorization

--- src/euler/primes.clj
+++ src/euler/primes.clj
@@ -9,23 +9,41 @@
   "Return a list of integer divisors of argument n"
   [n]
   (letfn [(divisors-helper [acc n i max]
-            (if (> n max) acc
-                (if (= (mod n i) 0)
+            (if (> i max) acc
+                (if (zero? (mod n i))
                   (divisors-helper (cons i acc) n (inc i) max)
                   (divisors-helper acc n (inc i) max))))]
-    (divisors-helper nil n 1 (round (floor (/ n 2))))))
-
+    (divisors-helper '() n 1 (Math/round (Math/floor (/ n 2))))))
 
 (defn num-divisors [n] (count (divisors n)))
 
 (defn sieve [n] nil)
 
-(defn factors
+(defn factor
   "Return a list of the integer factors of argument n"
   [n]
-  nil)
-
-
+  (letfn [(factor-helper [acc f rem]
+            (do
+              (println acc f rem)
+              (let [
+                    next (quot rem f)
+                    stop? (<= rem f)
+                    branch? (zero? (mod rem f))
+                    nf (if (= f 2) 3 (+ f 2))
+                    ]
+                  (if stop?
+                    (cons f acc)
+                    (if branch?
+                      (factor-helper (cons f acc) f next)
+                      (factor-helper acc nf rem)
+                      )
+                    )
+                  )
+                )
+            )
+          ]
+    (factor-helper '(1) 2 n))
+  )
 
 (defn phi [n] nil)
 
