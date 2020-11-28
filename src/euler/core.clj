b67064a19f40d3db3f9fd922c1d3a8b9a081eeba
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2020-11-28 10:56:02 +0100

    core: add count-hash

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,12 +1,23 @@
 (ns euler.core)
 
-(defn fib-seq
-  "Laxy fibonacci sequence generator"
+(defn fib-seq "Laxy fibonacci sequence generator"
   ([]
      (fib-seq 0 1))
   ([a b]
      (lazy-seq
       (cons b (fib-seq b (+' a b))))))
+
+(defn count-hash "Return a hash-map of {item => occurrences} on a supplied list"
+  ([l] (count-hash {} l))
+  ([acc l]
+   (if (empty? l) acc
+       (let [[head & tail] l]
+         (if (get acc head)
+           (recur (merge acc {head (inc (get acc head))}) tail)
+           (recur (conj {head 1} acc) tail)
+           )
+         )
+   )))
 
 (defn sum-digits [n] nil)
 
