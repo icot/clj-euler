29e805d390e0bdf286660d4fcc88dc6fdf55a868
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2020-12-14 13:40:08 +0100

    wip: p96

--- scripts/p96.clj
+++ scripts/p96.clj
@@ -3,23 +3,45 @@
   )
 
 ;; load input
-(def sudokus
+(def grids
   (partition 9 (filter #(not (str/includes? % "Grid"))
                        (str/split-lines (slurp "resources/p096_sudoku.txt")))))
 
-(println (first sudokus))
+(println (first grids))
 
 ;; Following Peter Norvig's sudoku solver
 ;; http://norvig.com/sudoku.html
+
+(defn cross [as bs] (for [a as b bs] (str/join "" (vector a b))))
+
+(defn in?
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
 
 (def digits '[\1 \2 \3 \4 \5 \6 \7 \8 \9])
 (def cols digits)
 (def rows '[\a \b \c \d \e \f \g \h \i])
 
-(def squares (for [r rows c cols] (vector r c)))
-
+(def squares (cross rows cols))
 
 (println squares)
+
+(def unitlist (concat
+               (for [c cols] (for [r rows] (str/join "" (vector r c)))) ; Columns
+               (for [r rows] (for [c cols] (str/join "" (vector r c)))) ; Rows
+               (for [rs '((\a \b \c) (\d \e \f) (\g \h \i)) cs '((\1 \2 \3) (\4 \5 \6) (\7 \8 \9))] (cross rs cs)) ; Boxes
+               )
+  )
+
+(def units (into (sorted-map) (for [s squares] (vector s (for [u unitlist :when (in? u s)] u)))))
+
+(def peers nil)
+
+;; Validations
+(assert (= (count squares) 81))
+(assert (= (count unitlist) 27))
 
 ;; display method
 ;;
