61f14433ab8e2a1eaa6a635b5935e776ebc51914
icot <icot@users.noreply.github.com>
2022-09-08 15:39:05 +0200

    p84: Refactored. Implement G2J, Streak. Missing Stats bug on chests  final positions

--- scripts/p84.clj
+++ scripts/p84.clj
@@ -1,6 +1,21 @@
-(ns p84
-  [:require [euler.core :as ec]])
+(ns p84)
 
+(def N 1000000)
+(def stats (atom {}))
+(def community-chest (atom (shuffle (range 1 17))))
+(def chance-chest (atom (shuffle (range 1 17))))
+(def streak (atom 0))
+(def cc-moves {1 0, 2 10})
+(def chance-moves {1 0,
+                   2 10,
+                   3 11,
+                   4 24
+                   5 39,
+                   6 5,
+                   7 #'next-railway
+                   8 #'next-railway
+                   9 #'next-utility
+                   10 #'go-back-3})
 
 (defn next-railway [pos]
   (cond
@@ -16,26 +31,16 @@
     12))
 
 (defn go-back-3 [pos] (mod (+ 40 pos -3) 40))
-
-(def cc-moves {1 0, 2 10})
-
-(def chance-moves {1 0,
-                   2 10,
-                   3 11,
-                   4 24
-                   5 39,
-                   6 5,
-                   7 #'next-railway
-                   8 #'next-railway
-                   9 #'next-utility
-                   10 #'go-back-3})
   
-(defn roll-dice [] (+ (inc (rand-int 6)) (inc (rand-int 6))))
+(defn roll-dice []
+  (let [d1 (inc (rand-int 6))
+        d2 (inc (rand-int 6))]
+    (do
+      (if (= d1 d2)
+        (swap! streak inc)
+        (swap! streak (fn [n] (* n 0))))
+      (+ d1 d2))))
 
-;(defn roll-dice [] 1)
-
-(def community-chest (atom (shuffle (range 1 17))))
-(def chance-chest (atom (shuffle (range 1 17))))
 
 (defn rotate [coll]
   (concat (rest coll) (list (first coll))))
@@ -58,12 +63,15 @@
         (apply card pos)
         draw))))
 
-(defn move [pos]
+(defn move [pos streak]
   (let [next-square (mod (+ pos (roll-dice)) 40)]
     (do
       (update-stats stats pos)
-      (update-stats stats next-square)
       (cond
+        (= @streak 3) (do
+                        (swap! streak (fn [n] (* n 0))) ; Reset streak
+                        10) ; And go to Jail
+        (= next-square 30) 10; G2J
         (some #(= next-square %) '(2 17 33)) (draw-from-community-chest pos)
         (some #(= next-square %) '(7 22 36)) (draw-from-chance-chest pos)
         :else next-square))))
@@ -75,11 +83,6 @@
     (swap! stats assoc pos 1)))
 
 ;; https://clojuredocs.org/clojure.core/sorted-map-by
-
-(def N 1000000)
-
-(def stats (atom {}))
-
 (defn display-stats [stats]
   (let [sorted-stats (into (sorted-map-by (fn [key1 key2]
                                             (compare [(get @stats key2) key2]
@@ -88,12 +91,16 @@
     (println modal (count sorted-stats))))
  
 (time (loop [n 0 pos 0]
+;        (display-stats stats)
+;        (println @streak)
         (when (< n N)
-          (let [next-pos (move pos)]
+          (let [next-pos (move pos streak)]
             (recur (inc n) next-pos)))))
 
 (display-stats stats)
 
-;; BUG: For 6 sides dice getting 001035
+(println "G2J :" (@stats 30)) ;; Should be nil
 
+;; TODO
+;; -> G2J
             
